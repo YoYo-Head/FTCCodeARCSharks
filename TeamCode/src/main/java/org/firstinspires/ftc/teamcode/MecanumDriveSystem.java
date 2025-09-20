ca2fc5ed8478913f2a49c533a1605ba38009fdb1
YoYo-Head <yoyohead438@gmail.com>
2025-09-21 09:09:38 +1000

    (v25.1.1) Comments and TeleOp Visibility
    * Added useful comments so people understand what parts do what
    * Added spaces for easy reading
    * Gave TeleOp in MecanumDriveSystem.java a name and group for visibility when choosing a TeleOp on the Android Driver Station

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveSystem.java
@@ -3,7 +3,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp
+@TeleOp(name="Mecanum Drive System", group="Drive System")
 public class MecanumDriveSystem  extends OpMode {
     // Creating the variable for the DcMotors
     DcMotor FLeft;
@@ -13,6 +13,7 @@ public class MecanumDriveSystem  extends OpMode {
 
     @Override
     public void init() {
+        // The deviceName needs to be changed to how it in driver station config thingy
         FLeft = hardwareMap.get(DcMotor.class, "motor1");
         FRight = hardwareMap.get(DcMotor.class, "motor2");
         BLeft = hardwareMap.get(DcMotor.class, "motor3");
@@ -21,17 +22,18 @@ public class MecanumDriveSystem  extends OpMode {
         //Inverting motors as they are opposite
         FLeft.setDirection(DcMotor.Direction.REVERSE);
         BLeft.setDirection(DcMotor.Direction.REVERSE);
+
     }
 
     @Override
     public void loop() {
         telemetry.addLine("Driving is now enabled /w Mecanum Drive");
+        // The actual driving part. drive is a function, as seen in the other part below this function
         drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
     }
 
     public void drive(double thrust, double strafe, double turn) {
-
         // This is the motor power formula for each driving motor
         double FLeftPower = thrust + strafe + turn;
         double FRightPower = thrust - strafe - turn;
@@ -40,11 +42,12 @@ public class MecanumDriveSystem  extends OpMode {
 
         double maxSpeed = 1.0;  // speed divisor
 
+        // This actually then inputs the powers for each motor
         FLeft.setPower(maxSpeed * FLeftPower);
         FRight.setPower(maxSpeed * FRightPower);
         BLeft.setPower(maxSpeed * BLeftPower);
         BRight.setPower(maxSpeed * BRightPower);
 
-
     }
 }
+// :0
