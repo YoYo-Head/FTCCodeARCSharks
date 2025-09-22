3d302b04556d3e4c0ce7eab9349bfaa09d654cd8
YoYo-Head <yoyohead438@gmail.com>
2025-09-22 20:59:23 +1000

    (v25.2.1) Camera Bugfixes and Line Cleaning
    * Added useful comments in CameraView.java
    * Fixed up Vision Portal breaking the code
    * Added loop in CameraView.java
    * Added line spacing for easy reading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraView.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraView.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-@TeleOp
+@TeleOp(name="Camera", group="Camera System")
 public class CameraView extends LinearOpMode {
     // Creating the camera
     WebcamName camera;
@@ -16,16 +14,43 @@ public class CameraView extends LinearOpMode {
     // Creating camera feed
     VisionPortal vision;
 
-    public void runOpMode() {
-        camera = hardwareMap.get(WebcamName.class, "Webcam1");
+    // Creating April Tag processor
+    AprilTagProcessor aprilTag;
 
+    @Override
+    public void runOpMode() {
+        // Activates function which defines and starts camera
+        initScanner();
+
+        if (opModeIsActive()) {
+            telemetry.addLine("Camera stream starting...");
+            while (opModeIsActive()) {
+                // This part of code keep running till the OpMode isn't active
+
+                // Share the CPU.
+                sleep(20);
+            }
+        }
+        // Stops the stream
+        vision.close();
+    }
+
+    public void initScanner() {
+        // Defines camera + finds camera on the Control Hub / Extension Hub
+        camera = hardwareMap.get(WebcamName.class, "Webcam1");
+        // Defines April Tag
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Defines the stream presets and starts stream
         vision = new VisionPortal.Builder()
                 .setCamera(camera)
-                .addProcessor(aprilTagProcessor)
+                .addProcessor(aprilTag)
                 .build();
 
+        // Sets active camera (idk if useless so might remove later)
         vision.setActiveCamera(camera);
 
     }
 
 }
+// Camera vision :0
